441966b7981f486fd2993208e5187cf2d5436d1d
baeheeji <baeheeji@users.noreply.github.com>
2025-09-21 23:29:02 +0900

    base

---
+++ src/test/java/com/inmyfridge/inmyfridge/InmyfridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmyfridge.inmyfridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmyfridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
